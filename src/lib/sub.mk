df9561166565028b76bd36fe7712ed59f6b2fe83
Alexey Laurentsyeu <alex@paidbsd.org>
2026-09-03 18:01:50 +0300

    Make the PIC switch actually work

--- src/lib/sub.mk
+++ src/lib/sub.mk
@@ -1,6 +1,7 @@
-EXTRAS:= \
-	nyetpci.so \
-	nyetpci.a
+EXTRAS:= nyetpci.a
+.if ${NYETPCI_PIC} == 1
+EXTRAS+= nyetpci.so
+.endif
 
 .include "${MBDIR}/extras.mk"
 
@@ -8,5 +9,7 @@ ${SUBOBJDIR}/nyetpci.a: ${OBJS_ALL}
 	${AR} rcs $@ $>
 	${RANLIB} $@
 
+.if ${NYETPCI_PIC} == 1
 ${SUBOBJDIR}/nyetpci.so: ${OBJS_ALL}
 	${LD} ${LDFLAGS} -shared -o $@ $>
+.endif
