db057c909b3951c6bf38002e4e60efcd048c2aee
Alexey Laurentsyeu <alex@paidbsd.org>
2026-09-03 17:59:06 +0300

    Add a PIC switch

--- mbconfig.mk
+++ mbconfig.mk
@@ -7,8 +7,16 @@ PLAT_REAL:= ${NYETPCI_PLAT:tu}
 PLAT_REAL:= ${.MAKE.OS:tu}
 .endif
 
+NYETPCI_PIC?= 1
+
 #
 # Toolchain flags
 #
-CFLAGS:= ${CFLAGS_ADD} -DNYETPCI_PLAT_${PLAT_REAL} -I${INCDIR} -Wall -Wextra -pedantic -std=c99 -Os -fPIC
+CFLAGS:= ${CFLAGS_ADD} -DNYETPCI_PLAT_${PLAT_REAL} -I${INCDIR} -Wall -Wextra -pedantic -std=c99 -Os
 LDFLAGS:= ${LDFLAGS_ADD} -L${OBJDIR}/lib -s
+
+.if ${NYETPCI_PIC} == 1
+CFLAGS+= -fPIC
+.else
+CFLAGS+= -fno-pic
+.endif
